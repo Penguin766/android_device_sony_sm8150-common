6cadf1f0bdb2791c8873b6ae55cf5ca510fe71c6
kyasu <kyasu.dev@gmail.com>
2025-09-06 16:03:50 +0900

    sm8150-common: Migrate QTI power HAL to soong_config_set
    * Set libpowermode extension lib via soong config
    
    Change-Id: I02e69b57f11e3cc1eb61d02bba65e97b119c0bbe

--- common.mk
+++ common.mk
@@ -319,7 +319,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
-# QMI
+$(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
+
+# QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor
 
