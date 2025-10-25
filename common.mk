0b6f1380a5daf4244a0f842179719b4cc1de4f51
Penguin766 <83493195+Penguin766@users.noreply.github.com>
2025-10-25 19:00:58 +0200

    sm8150-common: Drop XperiaParts
    Depends on LineageOS specific HALs that PixelOS doesn't ship.

--- common.mk
+++ common.mk
@@ -419,7 +419,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
-
-# XperiaParts
-PRODUCT_PACKAGES += \
-    XperiaParts
